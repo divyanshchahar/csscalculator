29e7bbc7aef342fde6672de2b37971a59e882854
divyanshchahar <divyanshchahar@outlook.com>
2022-11-04 17:49:46 +0530

    added layout for top-nav-bar

---
+++ client/src/layouts/TopNavBar.js
@@ -0,0 +1,13 @@
+import BrandingContainer from "../components/BrandingContainer";
+import ExportCssButton from "../components/ExportCssButton";
+
+function TopNavBar() {
+  return (
+    <div classNAme="top-nav-bar">
+      <BrandingContainer />
+      <ExportCssButton />
+    </div>
+  );
+}
+
+export default TopNavBar;
