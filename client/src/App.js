712cd5017f894b42d6fa8e4d678a943cf1069b39
divyanshchahar <divyanshchahar@outlook.com>
2022-11-07 15:50:15 +0530

    integrated new functionality in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import TopNavBar from "./layouts/TopNavBar";
+import ListOfVar from "./layouts/ListOfVar";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <TopNavBar />
+      <ListOfVar />
     </>
   );
 }
