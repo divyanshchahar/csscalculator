98dfe02845060a48a8a5734775341bcfcfe16558
divyanshchahar <divyanshchahar@outlook.com>
2022-11-15 21:36:00 +0530

    added to functiionality to append variable to local storage

---
+++ client/src/utils/WriteVarToLocal.js
@@ -0,0 +1,10 @@
+function WriteVarToLocal(arg) {
+  const [varName, varValue] = arg;
+  const obj = JSON.parse(window.localStorage.getItem("CSS_CACLCULATOR"));
+
+  const temp = { ...obj, ...{ [varName]: varValue } };
+
+  window.localStorage.setItem("CSS_CACLCULATOR", JSON.stringify(temp));
+}
+
+export default WriteVarToLocal;
