024700c471a8918b163d798c760d241776e06f1b
divyanshchahar <divyanshchahar@outlook.com>
2022-11-15 15:29:25 +0530

    removed un necessary code

--- client/src/utils/AddVarToLocal.js
+++ client/src/utils/AddVarToLocal.js
@@ -2,7 +2,6 @@ import CheckLocalStorage from "./CheckLocalStorage";
 import InitializeLocalStorage from "./InitializeLocalStorage";
 
 function AddVarToLocal(arg) {
-  console.log(CheckLocalStorage());
   if (CheckLocalStorage()) {
     // code yet to follow
   } else {
