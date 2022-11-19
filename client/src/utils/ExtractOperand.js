18a4072dab27cd33a0031704d6ad958cd8048c9c
divyanshchahar <divyanshchahar@outlook.com>
2022-11-19 16:15:38 +0530

    added code to ExtractOperand.js

---
+++ client/src/utils/ExtractOperand.js
@@ -0,0 +1,39 @@
+function ExtractOperand(arg) {
+  const [operand1, calcString] = arg;
+
+  const decimalString = /\./g;
+  const intAfterOperand = /[\*\+\-\/]\s\d+/g;
+  const intBeforeOperand = /\d+\s[\*\+\-\/]/g;
+  const decimalBeforeOperand = /\d+\.\d+\s[\*\+\-\/]/g;
+  const decimalAfterOperand = /[\*\+\-\/]\s\d+.\d+/g;
+  const wholeOperand = /\d+/g;
+  const decimalOperand = /\d+\.\d+/g;
+
+  if (decimalString.test(calcString)) {
+    if (decimalBeforeOperand.test(arg)) {
+      let [temp] = calcString.match(decimalBeforeOperand);
+      [temp] = temp.match(decimalOperand);
+      return [operand1, temp.trim()];
+    }
+
+    if (decimalAfterOperand.test(arg)) {
+      let [temp] = calcString.match(decimalAfterOperand);
+      [temp] = temp.match(decimalOperand);
+      return [operand1, temp.trim()];
+    }
+  } else {
+    if (intAfterOperand.test(arg)) {
+      let [temp] = calcString.match(intAfterOperand);
+      [temp] = temp.match(wholeOperand);
+      return [operand1, temp.trim()];
+    }
+
+    if (intBeforeOperand.test(arg)) {
+      let [temp] = calcString.match(intBeforeOperand);
+      [temp] = temp.match(wholeOperand);
+      return [operand1, temp.trim()];
+    }
+  }
+}
+
+export default ExtractOperand;
