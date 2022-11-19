ebdf4d6bbc7b39c7524693de5a8ec037282851dd
divyanshchahar <divyanshchahar@outlook.com>
2022-11-19 18:06:30 +0530

    added docstrings to GetVarFromLocal.js

--- client/src/utils/GetVarFromLocal.js
+++ client/src/utils/GetVarFromLocal.js
@@ -1,3 +1,9 @@
+/**
+ * retrives the value of a variable from local storage
+ * @param {string} arg - name of the variable to be extracted from local storage
+ * @returns {string} value of the variable stored in local storage
+ */
+
 function GetVarFromLocal(arg) {
   const obj = JSON.parse(window.localStorage.getItem("CSS_CACLCULATOR"));
 
