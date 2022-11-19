a73397837603c82ed054b81097a60613cc1babb1
divyanshchahar <divyanshchahar@outlook.com>
2022-11-19 17:29:36 +0530

    added docstrings to GenerateValueString.js

--- client/src/utils/GenerateValueString.js
+++ client/src/utils/GenerateValueString.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param {array} arg - array of calaulated value and unit of the refrenced value
+ * @returns {string} - a string formed by combining calculated value and unit of refrence value
+ */
 function GenerateValueString(arg) {
   const [calculatedValue, varUnit] = arg;
 
