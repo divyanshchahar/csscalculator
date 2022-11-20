b2d439af7424172bc7d123af1884eb108a7d55b0
divyanshchahar <divyanshchahar@outlook.com>
2022-11-20 19:08:34 +0530

    AddVarToLocal.js

--- client/src/utils/AddVarToLocal.js
+++ client/src/utils/AddVarToLocal.js
@@ -2,6 +2,11 @@ import CheckLocalStorage from "./CheckLocalStorage";
 import InitializeLocalStorage from "./InitializeLocalStorage";
 import UpdateLocalStorage from "./UpdateLocalStorage";
 
+/**
+ * function to add variable to local storage
+ * @param {string} arg - string containing variable name and variable value
+ */
+
 function AddVarToLocal(arg) {
   if (CheckLocalStorage()) {
     UpdateLocalStorage(arg);
