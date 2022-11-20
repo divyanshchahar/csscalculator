5bc642726adef22cde5ca89e0441ecccea392dc2
divyanshchahar <divyanshchahar@outlook.com>
2022-11-20 19:10:57 +0530

    added docstrings to UpdateLocalStorage.js

--- client/src/utils/UpdateLocalStorage.js
+++ client/src/utils/UpdateLocalStorage.js
@@ -2,6 +2,11 @@ import CheckVarInLocal from "./CheckVarInLocal";
 import AppendVarToLocal from "./AppendVarToLocal";
 import UpdateVarInLocal from "./UpdateVarInLocal";
 
+/**
+ * function to update variable in local storage
+ * @param {string} arg variable name
+ */
+
 function UpdateLocalStorage(arg) {
   if (CheckVarInLocal(arg)) {
     UpdateVarInLocal(arg);
