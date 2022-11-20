d471ba03cee5db3481f6e24aa605bec369ddc8ae
divyanshchahar <divyanshchahar@outlook.com>
2022-11-20 17:23:25 +0530

    add docstring ProcessAbsoluteVars.js

--- client/src/utils/ProcessAbsoluteVars.js
+++ client/src/utils/ProcessAbsoluteVars.js
@@ -1,3 +1,9 @@
+/**
+ * splits string varible name an variable value
+ * @param {string} arg string in form __--varname: varvalue__
+ * @returns {array} array containing varaible name and varaible value
+ */
+
 function ProcessAbsoluteVars(arg) {
   let [varName, temp] = arg.split(":");
 
