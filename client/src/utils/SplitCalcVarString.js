60d898dc653ab3602f928c569f8429189eb707cb
divyanshchahar <divyanshchahar@outlook.com>
2022-11-20 17:55:07 +0530

    modified docstrings in SplitCalcVarString.js

--- client/src/utils/SplitCalcVarString.js
+++ client/src/utils/SplitCalcVarString.js
@@ -1,5 +1,5 @@
 /**
- *
+ * splits string cooming form calculated variable
  * @param {string} arg - string in the form _--varname: calc(__calculation__);_
  * @returns {array} - array contaning variable name and calculation string in the for _calc(__calculation__)_
  */
