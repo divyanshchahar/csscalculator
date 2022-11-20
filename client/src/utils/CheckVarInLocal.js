d57961d9c52325f5e9de35e572343ba184740e1a
divyanshchahar <divyanshchahar@outlook.com>
2022-11-20 15:41:23 +0530

    added docstring to CheckVarInLocal.js

--- client/src/utils/CheckVarInLocal.js
+++ client/src/utils/CheckVarInLocal.js
@@ -1,3 +1,9 @@
+/**
+ * checks the presence of variable in lolcal storage, return tue if the variable is present otherwise returns false
+ * @param {array} arg - variable name and variable value
+ * @returns {boolean}
+ */
+
 function CheckVarInLocal(arg) {
   const [varName, varValue] = arg;
   const obj = JSON.parse(window.localStorage.getItem("CSS_CACLCULATOR"));
