f1c9147732df6b2cf05b603e47b9ac0bd37e5314
divyanshchahar <divyanshchahar@outlook.com>
2022-11-20 15:38:06 +0530

    added docstrings in CheckUnits.js

--- client/src/utils/CheckUnits.js
+++ client/src/utils/CheckUnits.js
@@ -1,3 +1,9 @@
+/**
+ * checks for valid css units, returns true if unit is valid, otherwise it return false
+ * @param {string} arg string containing variable quantity and unit
+ * @returns
+ */
+
 function CheckUnits(arg) {
   const pattern1 =
     /\d*(vh|vw|cm|mm|in|px|pt|pc|em|ex|ch|rem|vw|vh|vmin|vamx|%)/;
