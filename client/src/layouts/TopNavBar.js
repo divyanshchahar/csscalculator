711a3562e4c76ae3023ace4d17090403cc3143ae
divyanshchahar <divyanshchahar@outlook.com>
2022-11-20 19:18:40 +0530

    added docstrings to TopNavBar.js

--- client/src/layouts/TopNavBar.js
+++ client/src/layouts/TopNavBar.js
@@ -1,6 +1,11 @@
 import BrandingContainer from "../components/BrandingContainer";
 import ExportCssButton from "../components/ExportCssButton";
 
+/**
+ * layout to render Top NAvigation Bar
+ * @returns {HTML componenets}
+ */
+
 function TopNavBar() {
   return (
     <div className="top-nav-bar">
