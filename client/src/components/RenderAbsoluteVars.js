2a3151f70da9276a3afc0f8fe0e4322a55aa9606
divyanshchahar <divyanshchahar@outlook.com>
2022-11-26 20:07:52 +0530

    changed RenderAbsoluteVars.js
    - changed arguments of AddVarToLocal
    - updated docstring

--- client/src/components/RenderAbsoluteVars.js
+++ client/src/components/RenderAbsoluteVars.js
@@ -4,13 +4,13 @@ import AddVarToLocal from "../utils/AddVarToLocal";
 /**
  * componenet to render absolute variable
  * @param {string} arg - string of absolute variable
- * @returns
+ * @returns {HTML} - HTML componenents to render absolute variables
  */
 
 function RenderAbsoluteVars(arg) {
   const [varName, varValue] = ProcessAbsoluteVars(arg.param);
 
-  AddVarToLocal([varName, varValue]);
+  AddVarToLocal(varName, varValue);
 
   return <div className="css-var absolute">{arg.param}</div>;
 }
