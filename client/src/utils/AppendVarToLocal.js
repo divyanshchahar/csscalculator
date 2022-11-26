61c8db345919abeb7f9b2dc074d0376e4fd42e95
divyanshchahar <divyanshchahar@outlook.com>
2022-11-26 19:34:00 +0530

    refactored AppendVarToLocal.js
    - updated arguments
    - updated docstrings

--- client/src/utils/AppendVarToLocal.js
+++ client/src/utils/AppendVarToLocal.js
@@ -1,10 +1,10 @@
 /**
  * appends varaibale anme and value to local storage
- * @param {array} arg - array of varaible anme and varaible value
+ * @param {string} varName - name of the variable
+ * @param {string} varValue - value of the variable eg _10vh_ , _5px_
  */
 
-function AppendVarToLocal(arg) {
-  const [varName, varValue] = arg;
+function AppendVarToLocal(varName, varValue) {
   const obj = JSON.parse(window.localStorage.getItem("CSS_CACLCULATOR"));
 
   const temp = { ...obj, ...{ [varName]: varValue } };
