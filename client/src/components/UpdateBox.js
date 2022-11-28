217787a57d74a5da3925f830df58f8ce0eb470a2
divyanshchahar <divyanshchahar@outlook.com>
2022-11-28 14:53:38 +0530

    changed UpdateBox.js
    - changed arguments
    - debugged spelling mistake of onChange
    - integrated functionality to update list items
    - updated docstring

--- client/src/components/UpdateBox.js
+++ client/src/components/UpdateBox.js
@@ -1,24 +1,33 @@
 import { useState, useEffect } from "react";
+import UpdateListItem from "../utils/UpdateListItem";
 
 /**
- *
- * @param {string} arg
- * @returns {HTML} input of type text for updating list items
+ * Componenet to take user input for updating list items
+ * @param {array} args - array consisting of `single state object` , `array of statse object`, `function to modify state object`, `state variable to render or hide text input`, `state function to vhnage state variable which will display or hide state variable`
+ * @returns {HTML} input of type text
  */
 
-function UpdateBox(arg) {
-  const [inputString, setInputString] = useState();
+function UpdateBox(args) {
+  const [item, stateVar, stateFunc, isEditable, setIsEditable] = args.param;
+
+  const [userInput, setUserInput] = useState();
 
   useEffect(() => {
-    setInputString(arg);
+    setUserInput(item.userInput);
   }, []);
 
   return (
     <input
       type="text"
-      value={inputString}
-      onChnage={(e) => {
-        setInputString(e.target.value);
+      value={userInput}
+      onChange={(e) => {
+        setUserInput(e.target.value);
+      }}
+      onKeyDown={(e) => {
+        UpdateListItem(e, item.id, stateVar, stateFunc);
+        if (e.key === "Enter") {
+          setIsEditable(!isEditable);
+        }
       }}
     />
   );
