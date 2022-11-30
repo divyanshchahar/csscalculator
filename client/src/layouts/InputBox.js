934f5e319a9b2d42439dbeb09bcdffe695df2430
divyanshchahar <divyanshchahar@outlook.com>
2022-11-30 12:27:06 +0530

    no idea what is changed

---
+++ client/src/layouts/InputBox.js
@@ -0,0 +1,32 @@
+import CreateListItem from "../utils/CreateListItem";
+
+import { useState } from "react";
+
+/**
+ * Component to take user input
+ * @returns {HTML} input of type text
+ */
+
+function InputBox(args) {
+  const [stateVar, stateFunc] = args.params;
+  const [inputString, setInputString] = useState();
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputString}
+        onChange={(e) => setInputString(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && e.target.value.length > 0) {
+            console.log(e.target.value);
+            CreateListItem(e.target.value, stateVar, stateFunc);
+            setInputString("");
+          }
+        }}
+      />
+    </>
+  );
+}
+
+export default InputBox;
