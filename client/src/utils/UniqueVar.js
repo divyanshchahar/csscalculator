ca53e505f542817d8d59c22b8f7e35f366bb234a
divyanshchahar <divyanshchahar@outlook.com>
2022-11-30 14:35:04 +0530

    added functionality to chech if a varaible is unique or not

---
+++ client/src/utils/UniqueVar.js
@@ -0,0 +1,17 @@
+/**
+ * Checks if the variable name is unique
+ * @param {*} varName
+ * @param {*} stateVars
+ * @returns {boolean} - `true` if the variable name is unique or `false` if the variable name is not unique
+ */
+function UniqueVar(varName, stateVars) {
+  let isUnique = true;
+  stateVars.map((item) => {
+    if (item.varName == varName) {
+      isUnique = false;
+    }
+  });
+  return isUnique;
+}
+
+export default UniqueVar;
