50620d111998f9776de414bfb9c79c778a690081
divyanshchahar <divyanshchahar@outlook.com>
2022-11-30 16:21:12 +0530

    added functionality to check existence of a variable

---
+++ client/src/utils/VarExists.js
@@ -0,0 +1,18 @@
+/**
+ * Checks if the variable name exists in the list of variable
+ * @param {string} varName - name of the variable
+ * @param {array} stateVars - array of state variables
+ * @returns {boolean} - `true` if the variable name is unique or `false` if the variable name is not unique
+ */
+
+function VarExists(varName, stateVars) {
+  let varPresent = "refrence varaible does not exists";
+  stateVars.map((item) => {
+    if (item.varName === varName) {
+      varPresent = true;
+    }
+  });
+  return varPresent;
+}
+
+export default VarExists;
