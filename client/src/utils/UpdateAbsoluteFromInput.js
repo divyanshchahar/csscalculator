eff316efc66a3c085539608319c45e0949b72e40
divyanshchahar <divyanshchahar@outlook.com>
2022-12-01 20:05:42 +0530

    added functionality to update absolute variables

---
+++ client/src/utils/UpdateAbsoluteFromInput.js
@@ -0,0 +1,53 @@
+import HasColon from "./HasColon";
+import HasSemiColon from "./HasSemiColon";
+import SplitByColon from "./SplitByColon";
+import ValidVar from "./ValidVar";
+import CheckUnits from "./CheckUnits";
+import GenerateErrorObject from "./GenerateErrorObject";
+import GenerateGoodObject from "./GenerateGoodObject";
+
+/**
+ * Function to update absolute variables
+ * @param {string} itemID
+ * @param {string} inputString
+ * @param {array} stateVar
+ * @param {function} stateFunc
+ * @returns
+ */
+function UpdateAbsoluteFromInput(itemID, inputString, stateVar, stateFunc) {
+  const check1 = HasColon(inputString);
+  if (check1 === true) {
+  } else {
+    stateFunc(GenerateErrorObject(itemID, inputString, check1, stateVar));
+    return;
+  }
+
+  const check2 = HasSemiColon(inputString);
+  if (check2 === true) {
+  } else {
+    stateFunc(GenerateErrorObject(itemID, inputString, check2, stateVar));
+    return;
+  }
+
+  const [varName, varValue] = SplitByColon(inputString);
+
+  const check3 = ValidVar(varName);
+  if (check3 === true) {
+  } else {
+    stateFunc(GenerateErrorObject(itemID, inputString, check3, stateVar));
+    return;
+  }
+
+  const check4 = CheckUnits(varValue);
+  if (check4 === true) {
+  } else {
+    stateFunc(GenerateErrorObject(itemID, inputString, check4, stateVar));
+    return;
+  }
+
+  stateFunc(
+    GenerateGoodObject(itemID, inputString, varName, varValue, stateVar)
+  );
+}
+
+export default UpdateAbsoluteFromInput;
