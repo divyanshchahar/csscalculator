ed1f070c092a877e3f20785674b8cf86749497ab
divyanshchahar <divyanshchahar@outlook.com>
2022-12-06 16:05:01 +0530

    added functionality to make api call to backend

---
+++ client/src/utils/HandleGenerateFile.js
@@ -0,0 +1,26 @@
+/**
+ * Function to make api call to generate file
+ */
+
+function HandleGenerateFile() {
+  const obj = JSON.parse(window.localStorage.getItem("CSS_CALCULATOR"));
+
+  try {
+    if (obj.length !== 0) {
+      fetch("/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(obj),
+      })
+        .then((response) => response.json())
+        .then((data) => {})
+        .catch((error) => {});
+    }
+  } catch (error) {
+    alert(error);
+  }
+}
+
+export default HandleGenerateFile;
