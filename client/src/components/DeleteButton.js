59acf54d51e282f2f406a18552dd05e2548c07e1
divyanshchahar <divyanshchahar@outlook.com>
2022-12-06 18:10:40 +0530

    updated docstring and addded comments

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,5 +1,14 @@
-import deleteIcon from "../assets/delete_icon.svg";
+// IMPORTING FUNCTIONALITY
 import DeleTeListItem from "../utils/DeleTeListItem";
+
+// IMPORTING ASSETS
+import deleteIcon from "../assets/delete_icon.svg";
+
+/**
+ * Componenet to render delete button
+ * @param {array} args array of `itemID`, `stateVar` and `stateFunc`
+ * @returns {HTML} - HTML to render delete button
+ */
 
 function DeleteButton(args) {
   const [itemID, stateVar, stateFunc] = args.params;
